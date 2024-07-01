e21a716bb373cc9318c71d21e587d31136399b09
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2024-07-01 20:57:24 -0300

    butons

--- man.js
+++ man.js
@@ -1,6 +1,7 @@
 function emailSend(){
     var params = {
         from_name : document.getElementById("fullName").value,
+        subject : document.getElementById("subject").value,
         email_id : document.getElementById("email_id").value,
         message : document.getElementById("message").value,
     }
